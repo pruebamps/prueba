499aaf2ac6655235db5dbf666f89732fe110f495
im Iron Man <pruebagit.mps@gmail.com>
2022-10-08 01:23:50 -0300

    primer commit

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fibo(cantidad):
+    a,b=0,1
+    serie= list()
+    for i in range(cantidad):
+        serie.append(a)
+        a,b=b,a+b
+    return serie
+cantidad= int(input("Indique la cantidad de numeros de la secuencia que desea generar: "))
+print(fibo(cantidad))
+
